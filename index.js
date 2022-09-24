b2a80db9ff18384961caa9475689046a4c13c34f
hamzaiqbal2000 <hiqbal.bscs18seecs@seecs.edu.pk>
2022-09-25 01:20:33 +0500

    added database and connected to the mongodb

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
-app.set('view engine', 'ejs')
+const dotenv = require('dotenv')
+dotenv.config()
 
+app.set('view engine', 'ejs')
 //routes
 app.use('/', require('./routes/login'))
+
+// mongodb connection
+const database = process.env.MONGOLAB_URI
+mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => console.log('database connect'))
+.catch(err => console.log(err));
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log('Server start for port:' + PORT))
